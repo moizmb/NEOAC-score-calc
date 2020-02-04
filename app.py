4505c2bc9e15522c2088100c2d8edf85c822087a
Moiz Mansur <moiz.mansur@mindbodyonline.com>
2020-02-04 10:44:52 +0530

    Outputs CSV for males and females separately after calculating normalized scores for NEOAC, IAT and PES

--- app.py
+++ app.py
@@ -1,14 +1,107 @@
+import os
 import glob
+import csv
 
 from openpyxl import load_workbook
 
+from score_calculator import (
+    NEOACScoreCalculator,
+    PESScoreCalculator,
+    IATScoreCalculator
+)
+
+result_path = 'data/Results'
+csv_header = [
+    'response_id',
+    'initials',
+    'age',
+    'gender',
+    'education',
+    'occupation',
+    'iat_score',
+    'pes_score',
+    'neoac_score_n',
+    'neoac_score_e',
+    'neoac_score_o',
+    'neoac_score_a',
+    'neoac_score_c',
+]
+
 def main():
-    response_files = glob.glob('./data/responses/*.xlsx')
+    '''
+    Function takes files from the data folder.
+    Sends excel files through score_aggregator that
+    get all three test scores.
 
-    for response_file in response_files:
-        wb = load_workbook(response_file)
+    Prints all the data to an excel file. This function
+    is a glue function
+    '''
+    gender_files = {
+        'male': glob.glob('data/Male/*.xlsx', recursive=True),
+        'female': glob.glob('data/Female/*.xlsx', recursive=True)
+    }
 
+
+    for gender, files in gender_files.items():
+        scores = []
+        gender_result_file = open('{}/{}.csv'.format(result_path, gender), 'w')
+        gender_csv_writer = csv.writer(gender_result_file)
+        gender_csv_writer.writerow(csv_header)
+
+        for score_file in files:
+            print('Processing {}'.format(score_file))
+
+            wb = load_workbook(score_file)
+            if len(wb.sheetnames) != 3:
+                print('Please check file {} for number of tabs'.format(score_file))
+
+            iat_column = list(wb['Sheet1'].columns)[0]
+            iat_responses = list(map(lambda x: x.value, iat_column))
+
+            pes_column = list(wb['Sheet2'].columns)[0]
+            pes_responses = {idx + 1: value for idx, value in enumerate(list(map(lambda x: x.value, pes_column)))}
+
+            neoac_column = list(wb['Sheet3'].columns)[0]
+            neoac_responses = {idx + 1: value for idx, value in enumerate(list(map(lambda x: x.value, neoac_column)))}
+
+            if (len(iat_column) != 20 or len(pes_column) != 9 or len(neoac_column) != 60):
+                print('Please check file {} for survey scores'.format(score_file))
+                next
+
+            response_id, initials, age, education, occupation = os.path.basename(score_file).split('.')[0].split('_')[:5]
+
+            iat_score = IATScoreCalculator(iat_responses).calculate_scores()
+            pes_score = PESScoreCalculator(pes_responses).calculate_scores()
+            neoac_score = NEOACScoreCalculator(neoac_responses, gender).calculate_scores()
+
+            csv_row = [
+                response_id,
+                initials,
+                age,
+                gender,
+                education,
+                occupation,
+                iat_score,
+                pes_score,
+                neoac_score['n'],
+                neoac_score['e'],
+                neoac_score['o'],
+                neoac_score['a'],
+                neoac_score['c'],
+            ]
+
+            gender_csv_writer.writerow(csv_row)
+
+        gender_result_file.close()
 
 
 if __name__ == '__main__':
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
+
     main()
+
+
+
+## Output of csv has to be in the form
+# ID, Initials, Age, Gender, N, E, O, A, C, PES, IAT
--- score_calculator.py
+++ score_calculator.py
@@ -1,7 +1,10 @@
-class ScoreCalculator:
+import json
+
+class NEOACScoreCalculator:
     '''
     Class takes responses from the NEO 5 Factor inventory profile
     and returns the NEOAC scores
+    Test No. 3
     '''
 
     SD, D, N, A, SA = 'SD', 'D', 'N', 'A', 'SA'
@@ -26,18 +29,18 @@ class ScoreCalculator:
         A: 1,
         SA: 0
     }
-    BACKWARDS_QUESTIONS = [1, 3, 8, 9, 12, 14, 15, 16, 18, 23, 24, 27, 29, 30, 31, 33, 38, 39, 42, 44, 45, 46, 48, 54, 55, 57, 59]
+    BACKWARDS_QUESTIONS = [
+        1, 3, 8, 9, 12,
+        14, 15, 16, 18,
+        23, 24, 27, 29,
+        30, 31, 33, 38,
+        39, 42, 44, 45,
+        46, 48, 54, 55,
+        57, 59
+    ]
+    RAW_TO_T_CONVERTER = json.load(open('./conversion/neo_raw_to_t.json', 'r'))
 
-    def __init__(self, responses):
-        self.responses = responses
-        self.n_scores = []
-        self.e_scores = []
-        self.o_scores = []
-        self.a_scores = []
-        self.c_scores = []
-
-
-    def calculate_raw_scores():
+    def __init__(self, responses, gender):
         '''
         Function calculates N, E, O, A and C scores
         for the NEO 5 Factor inventory profile
@@ -47,32 +50,128 @@ class ScoreCalculator:
             {
                 question_number: 'SD / D / N / A / SA'
             }
-        Response(Dict): Dict with the 5 raw scores for each of the personality traits
+            gender(Str): 'male', 'female' or 'combined'
+        Response(Dict): Dict with the 5 T scores for each of the personality traits
         '''
 
-        for question_id, response in self.responses:
-            response = RESPONSE_MAP[response]
-
-            if question_id in BACKWARDS_QUESTIONS:
-                score = BACKWARD_SCORE_ARRAY[response]
-            else:
-                score = FORWARD_SCORE_ARRAY[response]
-
-        if question_id % 5 == 1:
-            self.n_scores.append(score)
-        elif question_id % 5 == 2:
-            self.e_scores.append(score)
-        elif question_id % 5 == 3:
-            self.o_scores.append(score)
-        elif question_id % 5 == 4:
-            self.a_scores.append(score)
-        else:
-            self.c_scores.append(score)
-
-        return {
-            n: sum(self.n_scores),
-            e: sum(self.e_scores),
-            o: sum(self.o_scores),
-            a: sum(self.a_scores),
-            c: sum(self.c_scores)
+        self.responses = responses
+        self.gender = gender
+        self.scores = {
+            'n': [],
+            'e': [],
+            'o': [],
+            'a': [],
+            'c': []
         }
+
+    def calculate_scores(self):
+        '''
+        Function calculates N, E, O, A and C scores
+        for the NEO 5 Factor inventory profile
+
+        '''
+
+        for question_id, response in self.responses.items():
+            response = self.__class__.RESPONSE_MAP[response]
+
+            if question_id in self.__class__.BACKWARDS_QUESTIONS:
+                score = self.__class__.BACKWARD_SCORE_ARRAY[response]
+            else:
+                score = self.__class__.FORWARD_SCORE_ARRAY[response]
+
+            if question_id % 5 == 1:
+                self.scores['n'].append(score)
+            elif question_id % 5 == 2:
+                self.scores['e'].append(score)
+            elif question_id % 5 == 3:
+                self.scores['o'].append(score)
+            elif question_id % 5 == 4:
+                self.scores['a'].append(score)
+            else:
+                self.scores['c'].append(score)
+
+        return self.raw_to_t({
+            'n': sum(self.scores['n']),
+            'e': sum(self.scores['e']),
+            'o': sum(self.scores['o']),
+            'a': sum(self.scores['a']),
+            'c': sum(self.scores['c'])
+        })
+
+    def raw_to_t(self, raw_scores):
+        t_scores = {
+            'n': None,
+            'e': None,
+            'o': None,
+            'a': None,
+            'c': None
+        }
+        gender_converter = self.__class__.RAW_TO_T_CONVERTER[self.gender]
+        for trait, score in raw_scores.items():
+            converted_score = gender_converter[trait].get(str(score), None)
+            if converted_score:
+                t_scores[trait] = converted_score
+            elif score > int(gender_converter[trait]['max']):
+                t_scores[trait] = gender_converter[trait][gender_converter[trait]['max']]
+            elif score < int(gender_converter[trait]['min']):
+                t_scores[trait] = gender_converter[trait][gender_converter[trait]['min']]
+
+        return t_scores
+
+class PESScoreCalculator:
+    '''
+    Class calculates psychological entitlement scale
+    scores from responses
+    Test No. 2
+    '''
+
+    FORWARD_SCORE_ARRAY = {
+        1: 1,
+        2: 2,
+        3: 3,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 7
+    }
+
+    BACKWARD_SCORE_ARRAY = {
+        1: 7,
+        2: 6,
+        3: 5,
+        4: 4,
+        5: 3,
+        6: 2,
+        7: 1
+    }
+
+    BACKWARDS_QUESTIONS = [5]
+
+    def __init__(self, responses):
+        # responses takes a dict in the form {question_code: response}
+        self.responses = responses
+
+    def calculate_scores(self):
+        scores = []
+        for question_code, response in self.responses.items():
+            if question_code in self.__class__.BACKWARDS_QUESTIONS:
+                score = self.__class__.BACKWARD_SCORE_ARRAY[response]
+            else:
+                score = self.__class__.FORWARD_SCORE_ARRAY[response]
+            scores.append(score)
+
+        return sum(scores)
+
+
+class IATScoreCalculator:
+    '''
+    Calculates scores for the Internet Addition Test
+    Test No. 1
+    '''
+
+    def __init__(self, responses):
+        # responses takes a list
+        self.responses = responses
+
+    def calculate_scores(self):
+        return sum(list(self.responses))
