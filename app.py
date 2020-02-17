8e64a46f956a3c3d51db4fc7b8191a7b0395298e
Moiz Mansur <moiz.mansur@mindbodyonline.com>
2020-02-18 00:36:29 +0530

    Adds function to read from the google response sheet

--- app.py
+++ app.py
@@ -27,9 +27,10 @@ csv_header = [
     'neoac_score_c',
 ]
 
-def main():
+def read_from_individual_excels():
     '''
     Function takes files from the data folder.
+    Files that are in the form of one excel per survey
     Sends excel files through score_aggregator that
     get all three test scores.
 
@@ -94,11 +95,76 @@ def main():
         gender_result_file.close()
 
 
+def read_from_google_form_responses():
+    '''
+    Read from the google form responses
+    '''
+
+    print("\n\nProcessing form responses\n\n")
+    conversion_map = {v: k for k, v in NEOACScoreCalculator.RESPONSE_MAP.items()}
+
+    wb = load_workbook('data/form_responses.xlsx')
+    result_file_name = 'data/Results/form_results.csv'
+
+    result_file = open(result_file_name, 'w')
+    csv_writer = csv.writer(result_file)
+    csv_writer.writerow(csv_header)
+
+    responses_sheet = wb[wb.sheetnames[0]]
+
+    max_col = responses_sheet.max_column
+    max_row = responses_sheet.max_row
+
+    for i in range(2, max_row + 1):
+        print('Processing row {}'.format(i))
+        row = []
+        for j in range(1, max_col + 1):
+            cell_obj = responses_sheet.cell(row = i, column = j)
+            row.append(cell_obj.value)
+
+        response_id = row[0]
+        initials = ''.join(list(map(lambda x: x[0:1], row[2].split(' '))))
+        education = row[3]
+        age = row[4]
+        gender = row[5]
+        occupation = row[6]
+
+        iat_responses = row[11:31]
+        pes_responses = {idx + 1: v for idx, v in enumerate(row[31:40])}
+        neoac_responses = {idx + 1: v for idx, v in enumerate(list(map(lambda x: conversion_map[x], row[40:100])))}
+
+        iat_score = IATScoreCalculator(iat_responses).calculate_scores()
+        pes_score = PESScoreCalculator(pes_responses).calculate_scores()
+        neoac_score = NEOACScoreCalculator(neoac_responses, gender).calculate_scores()
+
+        csv_row = [
+            response_id,
+            initials,
+            age,
+            gender,
+            education,
+            occupation,
+            iat_score,
+            pes_score,
+            neoac_score['n'],
+            neoac_score['e'],
+            neoac_score['o'],
+            neoac_score['a'],
+            neoac_score['c'],
+        ]
+
+        csv_writer.writerow(csv_row)
+
+    result_file.close()
+
+
+
 if __name__ == '__main__':
     if not os.path.exists(result_path):
         os.makedirs(result_path)
 
-    main()
+    read_from_individual_excels()
+    read_from_google_form_responses()
 
 
 
